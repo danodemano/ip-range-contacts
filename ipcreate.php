f8e31860d201d73fc1a251aab851f16868af082f
danodemano <danodemano@gmail.com>
2015-04-11 21:21:26 -0400

    Update ipcreate.php

--- ipcreate.php
+++ ipcreate.php
@@ -1,7 +1,23 @@
 <?php
+
+//Get the op variable
+@$op = $_GET['op'];
+
+//Start the session, restarting it if the op isn't set
+session_start();
+if ($op=='') {
+	session_destroy();
+	session_start();
+}else{
+	//Get the session data
+	$ipaddress	= $_SESSION['ipaddress'];
+	$cidr 		= $_SESSION['cidr'];
+	$company 	= $_SESSION['company'];
+} //if ($op<>'') {
+
 //Get the configs and connect to the database
-require_once('lib/dbconfig.php');
-require_once('lib/dbconnect.php');
+//require_once('lib/dbconfig.php');
+//require_once('lib/dbconnect.php');
 ?>
 <html>
 <head>
@@ -9,19 +25,19 @@ require_once('lib/dbconnect.php');
 </head>
 <body>
 
-<form action='lib/doipcreate.php'>
+<form method="post" action='lib/doipcreate.php'>
 	<table>
 		<tr>
 			<td>IP&nbsp;Address:&nbsp;</td>
-			<td><input type="text" name="ip" id="ip" length="50"></td>
+			<td><input type="text" name="ip" id="ip" length="50" <?php if (!empty($ipaddress)) { echo 'value='.$ipaddress; }?>></td>
 		</tr>
 		<tr>
 			<td>CIDR:&nbsp;</td>
-			<td><input type="text" name="cidr" id="cidr" length="50"></td>
+			<td><input type="text" name="cidr" id="cidr" length="50" <?php if (!empty($cidr)) { echo 'value='.$cidr; }?>></td>
 		</tr>
 		<tr>
 			<td>Company:&nbsp;</td>
-			<td><input type="text" name="company" id="company" length="50"></td>
+			<td><input type="text" name="company" id="company" length="50" <?php if (!empty($company)) { echo 'value='.$company; }?>></td>
 		</tr>
 		<tr>
 			<td><input type="submit" value="Submit"></td>
@@ -31,5 +47,5 @@ require_once('lib/dbconnect.php');
 </form>
 <?
 //Close the database connection
-require_once('lib/dbclose.php');
+//require_once('lib/dbclose.php');
 ?>
