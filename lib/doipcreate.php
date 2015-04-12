64b42056436162a1ac477a48feaa2627dfef4e80
danodemano <danodemano@gmail.com>
2015-04-11 21:56:37 -0400

    Update doipcreate.php

--- lib/doipcreate.php
+++ lib/doipcreate.php
@@ -54,8 +54,54 @@ $validipv4 = filter_var($ipaddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
 $validipv6 = filter_var($ipaddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
 if ($validipv4) {
 	//Valid IPv4 - proceed!
+	
+	//Create the new block and get the first and last IP addresses
+	$block = new IPv4Block($ipaddress, $cidr);
+	$first = $block->getFirstIp();
+	$last  = $block->getLastIp();
+
+	//Convert the first IP to integer
+	$firstip  = new IPv4((string)$first);
+	$firstint = $firstip->numeric();
+
+	//Convert the last IP to integer
+	$lastip  = new IPv4((string)$last);
+	$lastint = $lastip->numeric();
+
+	//SANATIZE ALL THE THINGS!!
+	$firstint = mysqli_real_escape_string($conn, $firstint);
+	$lastint  = mysqli_real_escape_string($conn, $lastint);
+	$cidr	  = mysqli_real_escape_string($conn, $cidr);
+	$company  = mysqli_real_escape_string($conn, $company);
+
+	//SQL query that will insert the required data into the database
+	$sql = "INSERT INTO `ip_ranges` (`start`, `end`, `cidr`, `company`, `ipv`) VALUES ('$firstint', '$lastint', '$cidr', '$company', '4');";
+	die ($sql);
 } else if ($validipv6) {
 	//Valid IPv6 - proceed!
+
+	//Create the new block and get the first and last IP addresses
+	$block = new IPv6Block($ipaddress, $cidr);
+	$first = $block->getFirstIp();
+	$last  = $block->getLastIp();
+
+	//Convert the first IP to integer
+	$firstip  = new IPv6((string)$first);
+	$firstint = $firstip->numeric();
+
+	//Convert the last IP to integer
+	$lastip  = new IPv6((string)$last);
+	$lastint = $lastip->numeric();
+
+	//SANATIZE ALL THE THINGS!!
+	$firstint = mysqli_real_escape_string($conn, $firstint);
+	$lastint  = mysqli_real_escape_string($conn, $lastint);
+	$cidr	  = mysqli_real_escape_string($conn, $cidr);
+	$company  = mysqli_real_escape_string($conn, $company);
+
+	//SQL query that will insert the required data into the database
+	$sql = "INSERT INTO `ip_ranges` (`start`, `end`, `cidr`, `company`, `ipv`) VALUES ('$firstint', '$lastint', '$cidr', '$company', '6');";
+	die ($sql);
 } else {
 	//Not a valid IP address, redirect back with an error
 	//Make sure we save all the data into the session
