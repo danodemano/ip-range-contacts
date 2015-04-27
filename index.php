311de9a04bd61898772e4f2e8d8b33ad30376a05
danodemano <danodemano@gmail.com>
2015-04-27 19:43:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
 <?php
+//Get the configs and connect to the database
+require_once('lib/dbconfig.php');
+require_once('lib/dbconnect.php');
 
+//Require the IP address handling library
+require_once('lib/ip.lib.php');
+
+//Require the main functions files
+require_once('lib/functions.php');
 ?>
+
+<html>
+<head>
+<title>IP Address Contacts - Home</title>
+</head>
+<body>
+<h2>Navigation Links:</h2>
+<a href="ipcreate.php">Create new IP block</a><br>
+<h2>Search for IP range (Company or Address):</h2>
+<form method="post" action='lib/doipsearch.php'>
+	<table>
+		<tr>
+			<td>Search&nbsp;Term:&nbsp;</td>
+			<td><input type="text" name="search" id="search" length="80"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" value="Submit"></td>
+			<td>&nbsp;</td>
+		</tr>
+	</table>
+</form>
+<h2>Current IP Blocks:</h2>
+<?php
+//Echo out all the IP blocks
+$ip_blocks = all_blocks($db);
+echo $ip_blocks;
+
+//Close the database connection
+require_once('lib/dbclose.php');
+?>
+</body>
+</html>
