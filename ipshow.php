095444004f7b0c0536e2df0a3b7efe2f903a4dd3
danodemano <danodemano@gmail.com>
2015-04-27 20:37:35 -0400

    Update ipshow.php

--- ipshow.php
+++ ipshow.php
@@ -1,3 +1,29 @@
 <?php
+//Get the configs and connect to the database
+require_once('lib/dbconfig.php');
+require_once('lib/dbconnect.php');
 
+//Require the IP address handling library
+require_once('lib/ip.lib.php');
+
+//Require the main functions files
+require_once('lib/functions.php');
+
+//Get the ID for the record
+$id = $_GET['id'];
+
+//Get the record for that ID
+$record = block_info($id, $db);
+
+//Close the database connection
+require_once('lib/dbclose.php');
 ?>
+<html>
+<head>
+<title>IP Address Contacts - Details</title>
+</head>
+<body>
+<h2>IP block details:</h2>
+<?php echo $record; ?>
+</body>
+</html>
