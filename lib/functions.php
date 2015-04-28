92fd47efdce16fa76db06a7a70c1f37a8bc5ee4d
danodemano <danodemano@gmail.com>
2015-04-28 08:55:22 -0400

    Update functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -2,13 +2,14 @@
 //This function is used to return all IP blocks
 function all_blocks ($db) {
   //Get all IP ranges from the database
-  $query = "SELECT `id`, `start`, `end`, `cidr`, `ipv`, `company` FROM `ip_ranges` WHERE 1 ORDER BY `company` ASC;";
+  $query = "SELECT `id`, `start`, `end`, `cidr`, `ipv`, `company`, `notes`, `provider` FROM `ip_ranges` WHERE 1 ORDER BY `company` ASC;";
   $res = $db->query($query);
   //Table header row creation
   $return_val = '<table border="1" cellpadding="5" cellspacing"10">'."\r\n".
                 "<tr><td><b>ID</b></td><td><b>Network</b></td>".
                 "<td><b>Broadcast</b></td><td><b>CIDR</b></td>".
-                "<td><b>IP&nbsp;Version</b></td><td><b>Company</b></td></tr>\r\n";
+                "<td><b>IP&nbsp;Version</b></td><td><b>Company</b></td>".
+				"<td><b>Notes</b></td><td><b>Provider</b></td></tr>\r\n";
   //Get all the records from the database
   foreach ($res as $records) {
     //Create each records row
@@ -31,6 +32,8 @@ function all_blocks ($db) {
     $return_val = $return_val . "<td>" . $records["cidr"] . "</td>\r\n";
     $return_val = $return_val . "<td>" . $records["ipv"] . "</td>\r\n";
     $return_val = $return_val . "<td>" . $records["company"] . "</td>\r\n";
+	$return_val = $return_val . "<td>" . $records["notes"] . "</td>\r\n";
+	$return_val = $return_val . "<td>" . $records["provider"] . "</td>\r\n";
     $return_val = $return_val . "</tr>\r\n";
   } //end foreach ($res as $records) {
 
@@ -44,8 +47,7 @@ function all_blocks ($db) {
 //This function gets the info for one block
 function block_info($id, $db){
   //Search for the block by ID passed to this function
-  $query = "SELECT `id`, `start`, `end`, `cidr`, `ipv`, `company` FROM `ip_ranges` WHERE `id` = ". $db->quote($id) ." LIMIT 1;";
-  //die ($query);
+  $query = "SELECT `id`, `start`, `end`, `cidr`, `ipv`, `company`, `notes`, `provider` FROM `ip_ranges` WHERE `id` = ". $db->quote($id) ." LIMIT 1;";
   $res = $db->query($query);
   //There should only be one record, retrieve it
   foreach ($res as $records) {
@@ -58,7 +60,9 @@ function block_info($id, $db){
                     "<b>Broadcast:</b>&nbsp;" . $broadcast  . "<br>\r\n".
                     "<b>CIDR:</b>&nbsp;" . $records['cidr']  . "<br>\r\n".
                     "<b>IP&nbsp;Version:</b>&nbsp;" . $records['ipv']  . "<br>\r\n".
-                    "<b>Company:</b>&nbsp;" . $records['company']  . "<br>\r\n";
+                    "<b>Company:</b>&nbsp;" . $records['company']  . "<br>\r\n".
+					"<b>Notes:</b>&nbsp;" . $records['notes']  . "<br>\r\n".
+					"<b>Provider:</b>&nbsp;" . $records['provider']  . "<br>\r\n";
     }else if ($records["ipv"] == '6') {
       //Convert the network and broadcast back to human formats
       $network = new IPv6($records["start"]);
@@ -68,7 +72,9 @@ function block_info($id, $db){
                     "<b>Broadcast:</b>&nbsp;" . $broadcast  . "<br>\r\n".
                     "<b>CIDR:</b>&nbsp;" . $records['cidr']  . "<br>\r\n".
                     "<b>IP&nbsp;Version:</b>&nbsp;" . $records['ipv']  . "<br>\r\n".
-                    "<b>Company:</b>&nbsp;" . $records['company']  . "<br>\r\n";
+                    "<b>Company:</b>&nbsp;" . $records['company']  . "<br>\r\n".
+					"<b>Notes:</b>&nbsp;" . $records['notes']  . "<br>\r\n".
+					"<b>Provider:</b>&nbsp;" . $records['provider']  . "<br>\r\n";
     } //end if ($records["ipv"] == '4') {
   } //end foreach ($res as $records) {
   return $return_val;
