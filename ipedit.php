daa3b1b634e59446071292e8b18efba246eb6b38
danodemano <danodemano@gmail.com>
2015-06-24 14:14:48 -0400

    Update ipedit.php

--- ipedit.php
+++ ipedit.php
@@ -18,6 +18,12 @@ $self = $_SERVER['PHP_SELF'];
 //Get the op variable
 @$op = $_GET['op'];
 
+//Get the record for that ID
+$record = block_details($id, $db);
+
+//Close the database connection
+require_once('lib/dbclose.php');
+
 //Start the session, restarting it if the op isn't set
 session_start();
 if ($op=='') {
@@ -25,8 +31,6 @@ if ($op=='') {
 	session_start();
 }else{
 	//Get the session data
-	$ipaddress	= $_SESSION['ipaddress'];
-	$cidr 		= $_SESSION['cidr'];
 	$company 	= $_SESSION['company'];
 	$notes		= $_SESSION['notes'];
 	$provider	= $_SESSION['provider'];
@@ -35,20 +39,43 @@ if ($op=='') {
 ?>
 <html>
 <head>
-<title>IP Address Contacts - Create Network</title>
+<title>IP Address Contacts - Edit Network</title>
+<link rel=stylesheet type="text/css" href="style.css">
 </head>
 <body>
-<h2>Use this form to enter in a new IP range and company</h2>
-<form method="post" action='lib/doipcreate.php'>
+<?php
+if ($record <> false) {
+//Valid ID - continue to display the information
+?>
+<h2>Use this form to edit the company/notes/provider for an IP block</h2>
+<p class="error">(NOTE: You cannot edit the network and CIDR.  If these need changed you will have to delete then recreate the network!)</p>
+<form method="post" action='lib/doipedit.php?id=<?php echo $id;?>'>
 	<table>
 		<tr>
-			<td>IP&nbsp;Address:&nbsp;</td>
-			<td><input type="text" name="ip" id="ip" length="50" <?php if (!empty($ipaddress)) { echo 'value='.$ipaddress; }?>></td>
+			<td>Network&nbsp;Address:&nbsp;</td>
+			<td><input type="text" name="ip" id="ip" length="50" <?php echo 'value="'.$record['network'].'"'; ?> readonly ></td>
 		</tr>
 		<tr>
 			<td>CIDR:&nbsp;</td>
-			<td><input type="text" name="cidr" id="cidr" length="50" <?php if (!empty($cidr)) { echo 'value='.$cidr; }?>></td>
+			<td><input type="text" name="cidr" id="cidr" length="50"  <?php echo 'value="'.$record['cidr'].'"'; ?> readonly ></td>
 		</tr>
+		<?php if ($op=='') {
+		?>
+		<tr>
+			<td>Company:&nbsp;</td>
+			<td><input type="text" name="company" id="company" length="50" <?php echo 'value="'.$record['company'].'"'; ?>></td>
+		</tr>
+		<tr>
+			<td>Notes:&nbsp;</td>
+			<td><input type="text" name="notes" id="notes" length="50" <?php echo 'value="'.$record['notes'].'"'; ?>></td>
+		</tr>
+		<tr>
+			<td>Provider:&nbsp;</td>
+			<td><input type="text" name="provider" id="provider" length="50" <?php echo 'value="'.$record['provider'].'"'; ?>></td>
+		</tr>
+		<?php
+		}else{
+		?>
 		<tr>
 			<td>Company:&nbsp;</td>
 			<td><input type="text" name="company" id="company" length="50" <?php if (!empty($company)) { echo 'value='.$company; }?>></td>
@@ -61,6 +88,7 @@ if ($op=='') {
 			<td>Provider:&nbsp;</td>
 			<td><input type="text" name="provider" id="provider" length="50" <?php if (!empty($provider)) { echo 'value='.$provider; }?>></td>
 		</tr>
+		<?php } //end <?php if ($op=='') { ?>
 		<tr>
 			<td><input type="submit" value="Submit"></td>
 			<td>&nbsp;</td>
@@ -70,6 +98,17 @@ if ($op=='') {
 <br>
 <br>
 <br>
+<?php
+}else{
+//No record found, display an error
+?>
+<h2><p class="error">IP BLOCK NOT FOUND!</p></h2>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php }// end if ($record <> false) { ?>
 <a href="index.php">Return to the main page</a>
 </body>
 </html>
