8811ac4961298b89e83a3556036f42aeed020ddd
danodemano <danodemano@gmail.com>
2015-06-24 14:32:06 -0400

    Update done.php

--- done.php
+++ done.php
@@ -4,7 +4,14 @@
 
 //Start the session
 session_start();
-
+?>
+<html>
+<head>
+<title>IP Address Contacts - Network Created!</title>
+<link rel=stylesheet type="text/css" href="style.css">
+</head>
+<body>
+<?php
 //If the op is null this page was likely accessed directly
 //Kill the session, print an error and die
 if ($op=='') {
@@ -28,13 +35,7 @@ if ($op=='') {
 	//Trash the session since we no longer need it
 	session_destroy();
 ?>
-<html>
-<head>
-<title>IP Address Contacts - Network Created!</title>
-<link rel=stylesheet type="text/css" href="style.css">
-</head>
-<body>
-<P class="success">The IP block has been created!</p>
+<p class="success">The IP block has been created!</p>
 Details follow:<br><br>
 Network: <b><?php echo $network; ?></b><br>
 Broadcast: <b><?php echo $broadcast; ?></b><br>
@@ -43,12 +44,32 @@ CIDR: <b><?php echo $cidr; ?></b><br>
 Company: <b><?php echo $company; ?></b><br>
 Notes: <b><?php echo $notes; ?></b><br>
 Provider: <b><?php echo $provider; ?></b><br>
-<br>
-<br>
-<br>
-Please <a href="index.php">click here</a> to return to the home page.
 <?php
-}else if ($op=='delete') {
+}else if ($op=='ipedited') {
+	//Make sure this page wasn't refreshed
+	if (empty($_SESSION['network'])) {
+		die("Don't do that.  ".'Please <a href="index.php">click here</a> to return home.');
+	} //end if (empty($_SESSION['network'])) {
+
+	//Get the session data
+	$network	= $_SESSION['network'];
+	$cidr 		= $_SESSION['cidr'];
+	$company 	= $_SESSION['company'];
+	$notes		= $_SESSION['notes'];
+	$provider	= $_SESSION['provider'];
+
+	//Trash the session since we no longer need it
+	session_destroy();
+?>
+<p class="success">The IP block has been edited!</p>
+Details follow:<br><br>
+Network: <b><?php echo $network; ?></b><br>
+CIDR: <b><?php echo $cidr; ?></b><br>
+Company: <b><?php echo $company; ?></b><br>
+Notes: <b><?php echo $notes; ?></b><br>
+Provider: <b><?php echo $provider; ?></b><br>
+<?php
+} else if ($op=='delete') {
 	//Trash the session since we no longer need it
 	session_destroy();
 ?>
@@ -57,11 +78,11 @@ Please <a href="index.php">click here</a> to return to the home page.
 <title>IP Address Contacts - Network Deleted!</title>
 </head>
 <body>
-<P class="success">The IP block has been Deleted!</p>
+<p class="success">The IP block has been Deleted!</p>
+<?php
+} //if ($op<>'') {
+?>
 <br>
 <br>
 <br>
 Please <a href="index.php">click here</a> to return to the home page.
-<?php
-} //if ($op<>'') {
-?>
