69f3af2868ff8069ba274565833d750cfbf278f1
danodemano <danodemano@gmail.com>
2015-06-24 10:37:36 -0400

    Update ipshow.php

--- ipshow.php
+++ ipshow.php
@@ -26,7 +26,7 @@ require_once('lib/dbclose.php');
 <h2>IP block details:</h2>
 <?php echo $record; ?>
 <br>
-<a href="edit.php?id=<?php echo $id; ?>">Edit&nbsp;Record</a>&nbsp;|&nbsp;<a href="delete.php?id=<?php echo $id; ?>">Delete&nbsp;Record</a><br>
+<a href="ipedit.php?id=<?php echo $id; ?>">Edit&nbsp;Record</a>&nbsp;|&nbsp;<a href="ipdelete.php?id=<?php echo $id; ?>">Delete&nbsp;Record</a><br>
 <br>
 <br>
 <br>
