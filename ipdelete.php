ed92e2143cd2e5e24849967bd57b608867dd452c
danodemano <danodemano@gmail.com>
2015-06-24 13:30:05 -0400

    Update ipdelete.php

--- ipdelete.php
+++ ipdelete.php
@@ -39,6 +39,10 @@ require_once('lib/dbclose.php');
 <link rel=stylesheet type="text/css" href="style.css">
 </head>
 <body>
+<?php
+if ($record <> false) {
+//Valid ID - continue to display the information
+?>
 <h2>IP block details:</h2>
 <?php echo $record; ?>
 <br>
@@ -46,6 +50,17 @@ require_once('lib/dbclose.php');
 <br>
 <br>
 <br>
+<?php
+}else{
+//No record found, display an error
+?>
+<h2><p class="error">IP BLOCK NOT FOUND!</p></h2>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php }// end if ($record <> false) { ?>
 <a href="index.php">Return to the main page</a>
 </body>
 </html>
